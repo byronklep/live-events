09ac89637ce4a2b238068178e8cad00780d6b12d
byronklep <byronklep@gmail.com>
2021-05-06 10:01:28 -0500

    Implemented Strapi.

--- components/Showcase.js
+++ components/Showcase.js
@@ -2,7 +2,7 @@ import styles from '@/styles/Showcase.module.css'
 
 export default function Showcase() {
   return (
-    <div className={styles.Showcase}>
+    <div className={styles.showcase}>
       <h1>Welcome To The Return of Live Music!</h1>
       <h2>Find the hottest Live Events</h2>
     </div>
