168ababaf34658951b4482e29a96659ba38a7cb4
byronklep <byronklep@gmail.com>
2021-05-05 21:21:47 -0500

    Module aliases & Showcase component.

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.Showcase}>
+      <h1>Welcome To The Return of Live Music!</h1>
+      <h2>Find the hottest Live Events</h2>
+    </div>
+  )
+}
